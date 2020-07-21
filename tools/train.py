d67b5bb0b7fa0e062c5f96eae0bb2388a680d014
Qianna <zhq9669@gmail.com>
2020-07-21 15:39:59 +0800

    none

--- tools/train.py
+++ tools/train.py
@@ -19,7 +19,7 @@ from mmdet.utils import collect_env, get_root_logger
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
     parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser.add_argument('--work_dir', help='the dir to save logs and models')
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
     parser.add_argument(
