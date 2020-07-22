0239ddf082d100ca3190546fd9735452f4566afc
Qianna <zhq9669@gmail.com>
2020-07-22 10:08:08 +0800

    none

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -6,5 +6,5 @@ GPUS=$3
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2201 \
     $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
