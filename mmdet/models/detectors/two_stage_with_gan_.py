03eea6fe5c70d68cb8a926d82ca0f8635e8ff6c9
Qianna <zhq9669@gmail.com>
2020-07-25 16:03:26 +0800

    modified for better to follow train stage 2

--- mmdet/models/detectors/two_stage_with_gan_.py
+++ mmdet/models/detectors/two_stage_with_gan_.py
@@ -157,7 +157,7 @@ class TwoStageGanDetector2(BaseDetector):
             self.rpn_head.eval()
             proposal_cfg = self.train_cfg.get('rpn_proposal',
                                               self.test_cfg.rpn)
-            proposal_list = self.rpn_head.simple_test_rpn(tuple(x_lr[1]), img_metas)
+            proposal_list = self.rpn_head.simple_test_rpn(tuple(x_lr[1],), img_metas)
             """rpn_losses, proposal_list = self.rpn_head.forward_train(
                 x,
                 img_metas,
