ad289de2ff6a8674af1d0073b46371150ab7940e
Qianna <zhq9669@gmail.com>
2020-07-26 16:30:45 +0800

    modified for better to follow train stage 2

--- mmdet/models/detectors/two_stage_with_gan_.py
+++ mmdet/models/detectors/two_stage_with_gan_.py
@@ -173,8 +173,10 @@ class TwoStageGanDetector2(BaseDetector):
             else:
                 proposal_list = proposals
 
-        roi_losses = self.roi_head.forward_train(x, img_metas, proposal_list, gt_bboxes, gt_labels,
-                                                 gt_bboxes_ignore, gt_masks, x_lr, **kwargs)
+        with torch.autograd.set_detect_anomaly(True):
+
+            roi_losses = self.roi_head.forward_train(x, img_metas, proposal_list, gt_bboxes, gt_labels,
+                                                     gt_bboxes_ignore, gt_masks, x_lr, **kwargs)
 
         losses.update(roi_losses)
 
