6fac04f31da775e4f726db5916d7ac67dd2c2558
Qianna <zhq9669@gmail.com>
2020-07-28 15:23:51 +0800

    none

--- mmdet/models/roi_heads/roi_head_with_gan.py
+++ mmdet/models/roi_heads/roi_head_with_gan.py
@@ -228,7 +228,6 @@ class RoIHeadGan(BaseRoIHead, BBoxTestMixin, MaskTestMixin):
         rois_index_hr = torch.where(areas > 96 * 96)
         rois_index_sr = torch.where(areas <= 96 * 96 * 4)
         rois_index_small = torch.where(areas <= 96 * 96)
-        print(list(rois_index_small[0]))
 
         bbox_results = self._bbox_forward(x, rois, rois_index_hr, rois_index_sr, rois_index_small, x_lr)
 
