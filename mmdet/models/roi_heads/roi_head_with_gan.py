dd8e9d328e41238e7768d8997b1a07b2606a2453
Qianna <zhq9669@gmail.com>
2020-07-28 10:43:03 +0800

    none

--- mmdet/models/roi_heads/roi_head_with_gan.py
+++ mmdet/models/roi_heads/roi_head_with_gan.py
@@ -191,11 +191,12 @@ class RoIHeadGan(BaseRoIHead, BBoxTestMixin, MaskTestMixin):
         # bbox_feats = self.bbox_roi_extractor(x, rois)
         if self.with_fsr_generator:
             bbox_feats_sub_hr, bbox_feats_hr = self.bbox_roi_extractor(x, rois)
-            print(type(bbox_feats_hr))
             bbox_feats_hr = self.fsr_generator((bbox_feats_sub_hr, bbox_feats_hr))
+            del bbox_feats_sub_hr
             if x_lr is not None:
                 bbox_feats_sub_lr, bbox_feats_lr = self.bbox_roi_extractor(x_lr, rois[rois_index_sr], for_lr=True)
                 bbox_feats_lr = self.fsr_generator((bbox_feats_sub_lr, bbox_feats_lr))
+                del bbox_feats_sub_lr
         """cls_score, bbox_pred = self.bbox_head(bbox_feats)
 
         bbox_results = dict(
@@ -208,6 +209,7 @@ class RoIHeadGan(BaseRoIHead, BBoxTestMixin, MaskTestMixin):
                 # bbox_feats_lr = self.shared_head(bbox_feats_sr[rois_index_small])
         # if x_lr is not None:
         cls_score, bbox_pred = self.bbox_head(bbox_feats[rois_index_small])
+        del bbox_feats
         bbox_results.update(cls_score=cls_score)
         bbox_results.update(bbox_pred=bbox_pred)
 
