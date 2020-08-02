897dbf25740ef501aca9c07ac6c840768f5a93f4
Qianna <zhq9669@gmail.com>
2020-08-02 21:34:36 +0800

    none

--- configs/fsr_sod/fsr_faster_rcnn_r50_caffe_c4_coco.py
+++ configs/fsr_sod/fsr_faster_rcnn_r50_caffe_c4_coco.py
@@ -189,3 +189,23 @@ lr_config_d = dict(
     step=[8, 11])
 
 total_epochs = 12
+
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
