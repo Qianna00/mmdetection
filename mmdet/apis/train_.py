24ad9300d6e8650c2ca1346c227e5b761133d381
Qianna <zhq9669@gmail.com>
2020-08-10 20:46:52 +0800

    none

--- mmdet/apis/train_.py
+++ mmdet/apis/train_.py
@@ -159,6 +159,5 @@ def train_detector_m(model,
         runner.resume(cfg.resume_from)
     elif cfg.load_from:
         runner.load_checkpoint(cfg.load_from)
-    print(runner.hooks)
     with torch.autograd.set_detect_anomaly(True):
         runner.run(data_loaders, cfg.workflow, cfg.total_epochs)
