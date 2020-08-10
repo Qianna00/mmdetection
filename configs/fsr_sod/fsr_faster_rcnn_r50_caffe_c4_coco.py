876abf0a7a941e3ce5a344e70e75d1973047d57d
Qianna <zhq9669@gmail.com>
2020-08-10 09:18:52 +0800

    none

--- configs/fsr_sod/fsr_faster_rcnn_r50_caffe_c4_coco.py
+++ configs/fsr_sod/fsr_faster_rcnn_r50_caffe_c4_coco.py
@@ -161,8 +161,8 @@ evaluation = dict(interval=1, metric='bbox')
 
 # optimizer
 optimizer_b = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
-optimizer_g = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
-optimizer_d = dict(type='SGD', lr=0.003, momentum=0.9, weight_decay=0.0001)
+optimizer_g = dict(type='SGD', lr=0.0005, momentum=0.9, weight_decay=0.0001)
+optimizer_d = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config_b = dict(grad_clip=None)
 optimizer_config_g = dict(grad_clip=None)
 optimizer_config_d = dict(grad_clip=None)
