83a20813a5534609b25636a1c3c6853c1c72637a
Qianna <zhq9669@gmail.com>
2020-08-12 17:23:22 +0800

    none

--- mmdet/models/roi_heads/standard_roi_head.py
+++ mmdet/models/roi_heads/standard_roi_head.py
@@ -149,6 +149,8 @@ class StandardRoIHead(BaseRoIHead, BBoxTestMixin, MaskTestMixin):
 
         bbox_targets = self.bbox_head.get_targets(sampling_results, gt_bboxes,
                                                   gt_labels, self.train_cfg)
+        print("bbox_pred:", bbox_results['bbox_pred'])
+        print("bbox_targets:", bbox_targets[2])
         loss_bbox = self.bbox_head.loss(bbox_results['cls_score'],
                                         bbox_results['bbox_pred'], rois,
                                         *bbox_targets)
