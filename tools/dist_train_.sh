e1431b7dbe7071aa66ff6130d22356011708b909
Qianna <zhq9669@gmail.com>
2020-08-16 07:31:01 +0800

    none

--- tools/dist_train_.sh
+++ tools/dist_train_.sh
@@ -5,5 +5,5 @@ GPUS=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2000 \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2001 \
     $(dirname "$0")/train_.py $CONFIG --launcher pytorch --work_dir /root/data/zq/mmdetection/outputs_temp ${@:3}
