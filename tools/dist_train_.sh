1ba41e73e147da13ec6f4ba06cd013e5ea178fce
Qianna <zhq9669@gmail.com>
2020-08-19 09:20:15 +0800

    none

--- tools/dist_train_.sh
+++ tools/dist_train_.sh
@@ -6,4 +6,5 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2001 \
-    $(dirname "$0")/train_.py $CONFIG --launcher pytorch --work_dir /root/data/zq/mmdetection/outputs_temp ${@:3}
+    $(dirname "$0")/train_.py $CONFIG --launcher pytorch --work_dir /root/data/zq/mmdetection/outputs_temp \
+    --resume-from /root/data/zq/mmdetection/outputs_temp/latest.pth ${@:3}
