98cd79e18f36b3defae9fb6c3dbcdfed52b1b8e5
Qianna <zhq9669@gmail.com>
2020-08-23 15:39:51 +0800

    none

--- configs/_base_/models/faster_rcnn_r50_caffe_c4.py
+++ configs/_base_/models/faster_rcnn_r50_caffe_c4.py
@@ -2,7 +2,7 @@
 norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNN',
-    pretrained='open-mmlab://detectron2/resnet50_caffe',
+    pretrained='/root/data/zq/mmdetection/checkpoints/resnet50_msra.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
