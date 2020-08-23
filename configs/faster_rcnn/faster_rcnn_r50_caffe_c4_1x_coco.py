cd00f03d328804483dfaba3b3a9e24fa1abd87f3
Qianna <zhq9669@gmail.com>
2020-08-23 16:08:36 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco.py
@@ -33,7 +33,7 @@ test_pipeline = [
 ]
 
 dataset_type = 'CocoDataset'
-data_root = '/root/data/zq/data/coco/'
+data_root = '/root/data/zq/data/SMD/'
 
 
 data = dict(
@@ -41,18 +41,18 @@ data = dict(
     workers_per_gpu=1,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
+        ann_file=data_root + 'annotations/Training/SMD_VIS_skip_2_train.json',
+        img_prefix=data_root + 'train/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'annotations/Training/SMD_VIS_skip_2_val.json',
+        img_prefix=data_root + 'train/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'annotations/Test/SMD_VIS_skip_2.json',
+        img_prefix=data_root + 'test/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
 # optimizer
