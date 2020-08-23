a8088ddaab898eef9387ac795658447303dff7fc
Qianna <zhq9669@gmail.com>
2020-08-23 15:27:14 +0800

    none

--- mmdet/models/roi_heads/standard_roi_head.py
+++ mmdet/models/roi_heads/standard_roi_head.py
@@ -149,7 +149,6 @@ class StandardRoIHead(BaseRoIHead, BBoxTestMixin, MaskTestMixin):
 
         bbox_targets = self.bbox_head.get_targets(sampling_results, gt_bboxes,
                                                   gt_labels, self.train_cfg)
-        print("labels:", bbox_targets[0].shape, bbox_targets[0])
         loss_bbox = self.bbox_head.loss(bbox_results['cls_score'],
                                         bbox_results['bbox_pred'], rois,
                                         *bbox_targets)
--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,5 +6,4 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2000 \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/data/zq/mmdetection/outputs1 \
-    --resume-from /root/data/zq/mmdetection/outputs1/latest.pth ${@:3}
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/data/zq/mmdetection/outputs_temp ${@:3}
