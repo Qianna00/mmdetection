849b10c2be42aacee2c68b2306c462ab9c32d350
Qianna <zhq9669@gmail.com>
2020-11-08 19:31:17 +0800

    none

--- configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage1.py
+++ configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage1.py
@@ -64,7 +64,7 @@ total_epochs = 12
 norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNNMetaEmbedding',
-    pretrained='open-mmlab://detectron2/resnet50_caffe',
+    pretrained='/root/data/zq/pretrained_models/resnet50_msra.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
