35d1370317a56421d03147bd1ec8f4ebcff06494
Qianna <zhq9669@gmail.com>
2020-11-18 20:39:30 +0800

    none

--- mmdet/models/detectors/two_stage_with_MetaEmbedding.py
+++ mmdet/models/detectors/two_stage_with_MetaEmbedding.py
@@ -252,7 +252,7 @@ class TwoStageDetectorMetaEmbedding(BaseDetector):
         # Calculate initial centroids only on training data.
         with torch.set_grad_enabled(False):
 
-            for inputs in tqdm(data):
+            for _, inputs in enumerate(data):
                 imgs, gt_labels, gt_bboxes, img_metas = inputs["img"], \
                                                         inputs["gt_labels"], \
                                                         inputs["gt_bboxes"],\
