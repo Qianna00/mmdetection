397ba084ac50b34ef2660a981b1dd3f6c79583a5
Qianna <zhq9669@gmail.com>
2020-11-19 09:44:08 +0800

    none

--- mmdet/models/detectors/two_stage_with_MetaEmbedding.py
+++ mmdet/models/detectors/two_stage_with_MetaEmbedding.py
@@ -290,6 +290,8 @@ class TwoStageDetectorMetaEmbedding(BaseDetector):
                 # Add all calculated features to center tensor
                 for i in range(len(labels)):
                     label = labels[i]
+                    print(bbox_feats[i].size())
+                    print(centroids[label.size()])
                     centroids[label] += bbox_feats[i]
 
         # Average summed features with class count
