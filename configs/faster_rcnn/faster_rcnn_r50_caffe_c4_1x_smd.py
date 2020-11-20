85a76ff7348ac2d980ca8ee9b01a077f5edc4a7c
Qianna <zhq9669@gmail.com>
2020-11-20 16:51:29 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
@@ -64,7 +64,7 @@ total_epochs = 12
 norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNN',
-    pretrained='/root/data/zq/mmdetection/checkpoints/resnet50_msra.pth',
+    pretrained='/root/data/zq/pretrained_models/resnet50_msra.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
