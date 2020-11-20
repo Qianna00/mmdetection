303f07fcf6f67f30ca399fdf4d8f67eb969ee355
Qianna <zhq9669@gmail.com>
2020-11-20 17:05:27 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
@@ -121,8 +121,7 @@ model = dict(
             reg_class_agnostic=False,
             loss_cls=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
-        init_centroids=False)
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))))
 # model training and testing settings
 train_cfg = dict(
     rpn=dict(
