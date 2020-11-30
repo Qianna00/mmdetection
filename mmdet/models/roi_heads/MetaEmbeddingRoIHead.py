ede8a2750f1d01516852770016224c87cea603a3
Qianna <zhq9669@gmail.com>
2020-11-30 22:25:07 +0800

    none

--- mmdet/models/roi_heads/MetaEmbeddingRoIHead.py
+++ mmdet/models/roi_heads/MetaEmbeddingRoIHead.py
@@ -74,7 +74,7 @@ class MetaEmbedding_RoIHead(nn.Module):
                                                                    gt_labels, self.std_roi_head.train_cfg)
         else:
             rois = bbox2roi(proposal_list)
-        # print(len(x))
+        print(x[0].shape)
         print(rois.size())
         bbox_feats = self.std_roi_head.bbox_roi_extractor(
             x[:self.std_roi_head.bbox_roi_extractor.num_inputs], rois)
