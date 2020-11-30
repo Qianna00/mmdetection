152c3face9d78a982039b1788fcb6e055c43a80a
Qianna <zhq9669@gmail.com>
2020-11-30 20:37:59 +0800

    none

--- configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
+++ configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
@@ -31,7 +31,7 @@ test_pipeline = [
         flip=False,
         transforms=[
             # dict(type='Resize', img_scale=(1920, 1080), keep_ratio=True),
-            dict(type='RandomFlip', flip_ratio=0.5),
+            dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
