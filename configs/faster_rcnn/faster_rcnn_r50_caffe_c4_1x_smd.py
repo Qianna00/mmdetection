a823b9dbc0c375974e0e2804e2bc5aa20fb9d64e
Qianna <zhq9669@gmail.com>
2020-11-30 15:36:17 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
@@ -174,7 +174,7 @@ test_cfg = dict(
         min_bbox_size=0),
     rcnn=dict(
         score_thr=0.05,
-        nms=dict(type='nms', iou_threshold=0.5),
+        nms=dict(type='nms', iou_thr=0.5),
         max_per_img=100))
 
 checkpoint_config = dict(interval=1)
