1cf540536460f8da2cfce8c5800369fa82ac9aee
Qianna <zhq9669@gmail.com>
2020-11-30 11:11:27 +0800

    none

--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -149,4 +149,5 @@ def show_result_pyplot(model, img, result, score_thr=0.3, fig_size=(15, 10)):
     img = model.show_result(img, result, score_thr=score_thr, show=False)
     plt.figure(figsize=fig_size)
     plt.imshow(mmcv.bgr2rgb(img))
-    plt.show()
+    plt.savefig("det_results_" + img)
+    # plt.show()
---
+++ my_code/single_image_test.py
@@ -0,0 +1,13 @@
+from mmdet.apis import inference_detector
+from mmdet.apis import init_detector
+from mmdet.apis import show_result_pyplot
+
+config_file = "../configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py"
+
+checkpoint_file = "/root/data/zq/smd_det/frcnn_res50_mmdet/10c/epoch_8.pth"
+
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+img = "/root/data/zq/smd_det/test_imgs/1.jpg"
+result = inference_detector(model, img)
+show_result_pyplot(model, img, result)
