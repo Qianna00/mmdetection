a062cad926e216af828ab18f3f68bf82196d5232
Qianna <zhq9669@gmail.com>
2020-12-03 10:59:38 +0800

    none

--- configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
+++ configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
@@ -16,7 +16,7 @@ train_pipeline = [
 pipeline_for_init_centroids = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1920, 1080), keep_ratio=True),
+    # dict(type='Resize', img_scale=(1920, 1080), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
