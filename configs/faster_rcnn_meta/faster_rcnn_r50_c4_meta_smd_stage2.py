1d14da38b426f6b622b84ce20b82c1003450dc5f
Qianna <zhq9669@gmail.com>
2020-12-04 15:25:43 +0800

    none

--- configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
+++ configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
@@ -64,14 +64,14 @@ centroids_cal=dict(
 evaluation = dict(interval=1, metric='bbox')
 
 # optimizer
-optimizer = dict(type='SGD', lr=[0.001, 0.001], momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
-    warmup_ratio=0.001,
+    warmup_ratio=0.0001,
     step=[8, 11])
 total_epochs = 12
 
