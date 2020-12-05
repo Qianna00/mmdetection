78ccb7d810daea223ca428a9b14ca82b2fdf252e
Qianna <zhq9669@gmail.com>
2020-12-05 09:49:24 +0800

    none

--- mmdet/models/losses/DiscCentroidsLoss.py
+++ mmdet/models/losses/DiscCentroidsLoss.py
@@ -59,7 +59,7 @@ class DiscCentroidsLoss(nn.Module):
         loss_repel = torch.clamp(margin - distmat_neg.sum() / (batch_size * self.num_classes * 14 * 14), 0.0, 1e6)
 
         # print(loss_repel, loss_attract)
-        loss_attract = 0.1 * loss_attract
+        # loss_attract = 0.1 * loss_attract
         loss_repel = 0.01 * loss_repel
         # loss = loss_attract + 0.01 * loss_repel
 
