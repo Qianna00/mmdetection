4d6e6f4292db31d3d435835ff77669527e96d7be
Qianna <zhq9669@gmail.com>
2020-12-22 21:33:48 +0800

    none

--- tools/train.py
+++ tools/train.py
@@ -136,7 +136,7 @@ def main():
     model = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
 
-    datasets = [build_dataset([cfg.data.train1, cfg.data.train2])]
+    datasets = [build_dataset(cfg.data.train)]
     if len(cfg.workflow) == 2:
         val_dataset = copy.deepcopy(cfg.data.val)
         val_dataset.pipeline = cfg.data.train.pipeline
