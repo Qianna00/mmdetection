25a8737254bca61fa189c7c8c05a76763327ccfa
Qianna <zhq9669@gmail.com>
2020-12-22 10:30:00 +0800

    none

--- mmdet/models/roi_heads/MetaEmbeddingRoIHead.py
+++ mmdet/models/roi_heads/MetaEmbeddingRoIHead.py
@@ -84,6 +84,7 @@ class MetaEmbedding_RoIHead(nn.Module):
                 bbox_feats_pos = bbox_feats[pos_index]
                 bbox_feats_pos = self.get_meta_embedding_feature(bbox_feats_pos, centroids)
                 bbox_feats[pos_index] = bbox_feats_pos
+                print("labels:", bbox_targets[0][pos_index])
                 loss_attract, loss_repel = self.loss_feat(bbox_feats_pos, bbox_targets[0][pos_index])
             else:
                 bbox_feats = self.get_meta_embedding_feature(bbox_feats, centroids)
