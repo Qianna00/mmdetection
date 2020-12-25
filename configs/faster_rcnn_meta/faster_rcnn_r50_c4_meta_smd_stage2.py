c36df117ea1f1bc8804b6b6b315f5c1eee8a0417
Qianna <zhq9669@gmail.com>
2020-12-25 14:49:21 +0800

    none

--- configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
+++ configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
@@ -110,7 +110,7 @@ model = dict(
         loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
     roi_head=dict(
         type='MetaEmbedding_RoIHead',
-        num_classes=10,
+        num_classes=6,
         feat_dim=1024,
         shared_head=dict(
             type='ResLayer',
@@ -131,7 +131,7 @@ model = dict(
             with_avg_pool=True,
             roi_feat_size=7,
             in_channels=2048,
-            num_classes=10,
+            num_classes=6,
             bbox_coder=dict(
                 type='DeltaXYWHBBoxCoder',
                 target_means=[0., 0., 0., 0.],
@@ -142,7 +142,7 @@ model = dict(
             loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
         loss_feat=dict(
             type="DiscCentroidsLoss",
-            num_classes=10,
+            num_classes=6,
             feat_dim=1024,
             size_average=True)),
     init_centroids=True)
