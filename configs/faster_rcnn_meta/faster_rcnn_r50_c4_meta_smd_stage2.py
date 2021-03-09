cb735e29895a4366a2519f095991fb1f8c8d61fb
Qianna <zhq9669@gmail.com>
2021-03-09 17:16:51 +0800

    none

--- configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
+++ configs/faster_rcnn_meta/faster_rcnn_r50_c4_meta_smd_stage2.py
@@ -79,7 +79,7 @@ total_epochs = 12
 norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNNMetaEmbedding',
-    pretrained='/root/data/zq/smd_det/meta_embedding/6/stage1/epoch_4_modified.pth',
+    pretrained='/root/data/zq/smd_det/meta_embedding/6c/stage1/epoch_10_modified.pth',
     # pretrained='/root/data/zq/pretrained_models/resnet50_msra.pth',
     backbone=dict(
         type='ResNet',
