f4001c4adf7972365f59a9bf4998c9c1069dd526
Qianna <zhq9669@gmail.com>
2021-03-15 21:58:45 +0800

    none

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -2,8 +2,9 @@ from .bbox_head import BBoxHead
 from .convfc_bbox_head import (ConvFCBBoxHead, Shared2FCBBoxHead,
                                Shared4Conv1FCBBoxHead)
 from .double_bbox_head import DoubleConvFCBBoxHead
+from .gs_bbox_head import GSBBoxHead
 
 __all__ = [
     'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
-    'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead'
+    'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'GSBBoxHead'
 ]
