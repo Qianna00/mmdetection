4db1e5c1d40ac023fbdd35d6e8e80e7e13601f4f
Qianna <zhq9669@gmail.com>
2021-03-16 16:14:07 +0800

    none

--- mmdet/datasets/smd.py
+++ mmdet/datasets/smd.py
@@ -501,7 +501,7 @@ class SmdDataset6Lvis(LvisDataset1, SmdDataset6):
 
         eval_results = {}
         # get original api
-        lvis_gt = self.coco
+        lvis_gt = self.lvis
         for metric in metrics:
             msg = 'Evaluating {}...'.format(metric)
             if logger is None:
@@ -559,7 +559,7 @@ class SmdDataset6Lvis(LvisDataset1, SmdDataset6):
                     for idx, catId in enumerate(self.cat_ids):
                         # area range index 0: all area ranges
                         # max dets index -1: typically 100 per image
-                        nm = self.coco.load_cats(catId)[0]
+                        nm = self.lvis.load_cats(catId)[0]
                         precision = precisions[:, :, idx, 0, -1]
                         precision = precision[precision > -1]
                         if precision.size:
