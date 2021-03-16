221ddc7ca163bd908610aa31330e382fec06e6ef
Qianna <zhq9669@gmail.com>
2021-03-16 15:57:08 +0800

    none

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,5 +6,5 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2001 \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2002 \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/data/zq/smd_det/meta_embedding/stage1 ${@:3}
