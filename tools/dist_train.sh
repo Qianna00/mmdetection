7f0498c7e0cdcd108ac062c0744caf8b41553e77
Qianna <zhq9669@gmail.com>
2021-06-10 18:30:47 +0800

    none

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,5 +6,5 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2010 \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/ikcest2021/bbox/retinanet/r50 ${@:3}
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2009 \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/ikcest2021/bbox/atss/r50 ${@:3}
