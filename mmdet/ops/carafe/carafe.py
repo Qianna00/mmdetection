46ba30785a726c2f4b467ebf037bdfdb1b5039cd
Qianna <zhq9669@gmail.com>
2021-06-17 15:35:55 +0800

    none

--- mmdet/ops/carafe/carafe.py
+++ mmdet/ops/carafe/carafe.py
@@ -157,7 +157,7 @@ class CARAFE(Module):
                                     self.group_size, self.scale_factor)
 
 
-@UPSAMPLE_LAYERS.register_module(name='carafe', force=True)
+@UPSAMPLE_LAYERS.register_module(name='carafe')
 class CARAFEPack(nn.Module):
     """ A unified package of CARAFE upsampler that contains:
     1) channel compressor 2) content encoder 3) CARAFE op
