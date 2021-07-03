89a4f6a20b82bd6dd4a6e49d0cbd0dd78166e5fa
Qianna <zhq9669@gmail.com>
2021-07-03 10:56:20 +0800

    none

--- my_code/anchor.py
+++ my_code/anchor.py
@@ -9,6 +9,8 @@ from sklearn.cluster import KMeans
 annotationFile = '/root/vsislab-2/zq/data/IKCEST3rd_bbox_detection/annotations/ikcest_train_bbox_annotations.json'
 # annotationFile = ''
 
+data_root = "/root/vsislab-2/zq/data/SMD_MOT/MOT16_format/images/train/"
+
 num_clusters = 9
 
 size = [360, 640]
@@ -25,15 +27,25 @@ print(size_list)"""
 # (1920,1080) (1280,720) (1920,1072) (1920,1088)
 
 bbox_list = []
-for i, ann in enumerate(annotations['annotations']):
+"""for i, ann in enumerate(annotations['annotations']):
     bbox = ann['bbox']
-    bbox_list.append(bbox[2:])
+    bbox_list.append(bbox[2:])"""
+for vid in os.listdir(data_root):
+    vid_path = os.path.join(data_root, vid)
+    gt_path = vid_path + 'gt/gt.txt'
+    with open(gt_path, 'r') as f:
+        gt_list = f.readlines()
+    for gt in gt_list:
+        bbox = gt.split(',')[4:6]
+        bbox_list.append(bbox)
+
 bboxes = np.array(bbox_list)
 
 kmeans = KMeans(n_clusters=num_clusters)
 anchors = kmeans.fit(bboxes)
 
 print(anchors.cluster_centers_)
+
 # [[ 819.76664478  637.74983585   53.064478     49.82744583]  53  50
 #  [  92.48940533  443.25112782  119.99822283  107.5393028 ]  120 108
 #  [ 628.43845922  391.9327415    43.9112248    42.28287692]  44  42
@@ -45,3 +57,19 @@ print(anchors.cluster_centers_)
 #  [ 463.01855406  630.64619322   74.21177223   62.1399019 ]] 74  62
 
 # [[44, 42], [52, 47], [53, 50], [66, 61], [74, 62], [78, 74], [109, 112], [120, 108], [407, 322]]
+
+# IKCEST2021
+# [[ 24.08195292  27.54004776]  24  28
+#  [326.28061582 214.93212036]  326 215
+#  [ 58.64028692  56.03207066]  59  56
+#  [ 78.09536576 157.22481808]  78  157
+#  [640.94308943 716.91056911]  641 717
+#  [120.47385664  80.50163806]  120 81
+#  [200.56102962 142.17077221]  201 142
+#  [217.4173913  350.16521739]  217 350
+#  [494.23175182 369.93978102]] 494 369
+
+# [[24, 28], [59, 56], [78, 157], [120, 81], [201, 142], [217, 350], [326, 215], [494, 369], [641, 717]]
+
+# SMD
+
