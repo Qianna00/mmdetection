c581ae894919e1118ee026fbc61a14c2e0ce7ec4
Qianna <zhq9669@gmail.com>
2021-07-15 00:13:27 +0800

    none

--- configs/faster_rcnn_unsup_embedding/faster_rcnn_unsup_embedding_smd.py
+++ configs/faster_rcnn_unsup_embedding/faster_rcnn_unsup_embedding_smd.py
@@ -64,7 +64,7 @@ total_epochs = 12
 norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNNUnsupEmbedding',
-    pretrained='/root/data/zq/mmdetection/checkpoints/resnet50_msra.pth',
+    pretrained='/root/data/zq/pretrained_models/resnet50_msra.pth',
     unsup_pretrained='/root/data/zq/pretrained_models/moco400k_epoch_50_modified.pth',
     backbone=dict(
         type='ResNet',
