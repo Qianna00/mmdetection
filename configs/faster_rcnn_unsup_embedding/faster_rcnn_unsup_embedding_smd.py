23834bf35b487f4cdc2b12a35890059231992286
Qianna <892878159@qq.com>
2021-07-16 17:00:54 +0800

    none

--- configs/faster_rcnn_unsup_embedding/faster_rcnn_unsup_embedding_smd.py
+++ configs/faster_rcnn_unsup_embedding/faster_rcnn_unsup_embedding_smd.py
@@ -193,7 +193,7 @@ test_cfg = dict(
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=1,
+    interval=20,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='TensorboardLoggerHook')
