15e127c389f741fde95343eb6d9b779b9bf096b9
Qianna <892878159@qq.com>
2021-07-17 21:51:16 +0800

    none

--- mmdet/models/roi_heads/UnsupEmbeddingRoIHead.py
+++ mmdet/models/roi_heads/UnsupEmbeddingRoIHead.py
@@ -163,8 +163,7 @@ class UnsupEmbedding_RoIHead(nn.Module):
         if target_labels is not None:
             for i in range(batch_size):
                 label = target_labels[i]
-                print(feats[i].size(), concept_selector.unsqueeze(2).unsqueeze(3).expand(-1, -1, feats.size(2), feats.size(3)).size())
-                feats[i] = feats[i] + concept_selector.unsqueeze(2).unsqueeze(3).expand(-1, -1, feats.size(2), feats.size(3)) * keys_memory[label]
+                feats[i] = feats[i] + concept_selector[i].unsqueeze(1).unsqueeze(2).expand(-1, feats.size(2), feats.size(3)) * keys_memory[label]
         else:
             distmat = (feats.clone().sum(dim=1, keepdim=True).expand(batch_size, self.num_classes, 14, 14) -
                        self.centroids.clone().sum(dim=1, keepdim=True)
@@ -172,8 +171,7 @@ class UnsupEmbedding_RoIHead(nn.Module):
             labels = distmat.argmin(dim=1)
             for i in range(batch_size):
                 label = labels[i]
-                feats[i] = feats[i] + concept_selector.unsqueeze(2).unsqueeze(3)\
-                    .expand(-1, -1, feats.size(2), feats.size(3)) * keys_memory[label]
+                feats[i] = feats[i] + concept_selector[i].unsqueeze(1).unsqueeze(2).expand(-1, feats.size(2), feats.size(3)) * keys_memory[label]
 
         """feats = direct_feature + concept_selector.unsqueeze(2).unsqueeze(3).expand(-1, -1, feats.size(2), feats.size(3))\
                 * memory_feature"""
