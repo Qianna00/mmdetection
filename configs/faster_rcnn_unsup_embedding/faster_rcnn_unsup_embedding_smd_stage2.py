23b7ef4b56affe9633adfdd6dd5f0e517f6b9265
Qianna <892878159@qq.com>
2021-07-28 15:59:45 +0800

    none

--- configs/faster_rcnn_unsup_embedding/faster_rcnn_unsup_embedding_smd_stage2.py
+++ configs/faster_rcnn_unsup_embedding/faster_rcnn_unsup_embedding_smd_stage2.py
@@ -80,7 +80,7 @@ norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNNUnsupEmbedding',
     pretrained='/root/vsislab-2/zq/smd_det/unsup_embedding/moco/temp/stage1/epoch_2.pth',
-    unsup_pretrained='/root/data/zq/pretrained_models/moco400k_epoch_50_modified.pth',
+    unsup_pretrained='/root/data/zq/pretrained_models/resnet50_marvel_35.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
