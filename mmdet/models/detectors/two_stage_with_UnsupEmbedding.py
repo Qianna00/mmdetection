1f7ed5311880b2e4bea6ff2041f529e65ffddd04
Qianna <892878159@qq.com>
2021-07-28 09:34:01 +0800

    none

--- mmdet/models/detectors/two_stage_with_UnsupEmbedding.py
+++ mmdet/models/detectors/two_stage_with_UnsupEmbedding.py
@@ -183,6 +183,7 @@ class TwoStageDetectorUnsupEmbedding(BaseDetector):
                     gt_labels=None,
                     gt_bboxes_ignore=gt_bboxes_ignore,
                     proposal_cfg=proposal_cfg)
+            proposal_list = proposal_list.detach()
             losses.update(rpn_losses)
         else:
             proposal_list = proposals
