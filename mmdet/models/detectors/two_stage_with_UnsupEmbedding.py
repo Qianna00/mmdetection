d89355125b5ce060d94fa1e0d79a3e6f7bce6078
Qianna <892878159@qq.com>
2021-08-03 17:29:48 +0800

    none

--- mmdet/models/detectors/two_stage_with_UnsupEmbedding.py
+++ mmdet/models/detectors/two_stage_with_UnsupEmbedding.py
@@ -90,7 +90,7 @@ class TwoStageDetectorUnsupEmbedding(BaseDetector):
                     dataset = build_dataset(cfg.centroids_cal)
                     # data = build_dataloader(dataset, samples_per_gpu=1, workers_per_gpu=0, num_gpus=1, shuffle=False)
                     # print(data[0])
-                    self.roi_head.loss_feat.centroids.data = self.centroids_cal(dataset)
+                    self.roi_head.centroids.data = self.centroids_cal(dataset)
 
     @property
     def with_rpn(self):
