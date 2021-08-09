814768732ce5292de4bdcf977231e1758ab2b9ff
Qianna <892878159@qq.com>
2021-08-09 20:51:20 +0800

    none

--- configs/faster_rcnn_unsup_embedding/faster_rcnn_r50_caffe_c4_1x_smd_concate_stage2.py
+++ configs/faster_rcnn_unsup_embedding/faster_rcnn_r50_caffe_c4_1x_smd_concate_stage2.py
@@ -49,7 +49,7 @@ data = dict(
 evaluation = dict(interval=1, metric='bbox')
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
@@ -61,32 +61,42 @@ lr_config = dict(
 total_epochs = 12
 
 # model settings
+norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
-    type='FasterRCNN',
-    pretrained='/root/data/zq/pretrained_models/moco400k_epoch_50_modified.pth',
+    type='FasterRCNNWithExtraBackbone',
+    pretrained='open-mmlab://detectron2/resnet101_caffe',
+    pretrained_extra='/root/data/zq/pretrained_models/resnet50_marvel_35.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
+        num_stages=3,
+        strides=(1, 2, 2),
+        dilations=(1, 1, 1),
+        out_indices=(2, ),
         frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_cfg=norm_cfg,
         norm_eval=True,
         style='caffe'),
-    neck=dict(
-        type='FPN',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        num_outs=5),
+    extra_backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=3,
+        strides=(1, 2, 2),
+        dilations=(1, 1, 1),
+        out_indices=(2,),
+        frozen_stages=3,
+        norm_cfg=norm_cfg,
+        norm_eval=True,
+        style='caffe'),
     rpn_head=dict(
         type='RPNHead',
-        in_channels=256,
-        feat_channels=256,
+        in_channels=1024,
+        feat_channels=1024,
         anchor_generator=dict(
             type='AnchorGenerator',
-            scales=[8],
+            scales=[2, 4, 8, 16, 32],
             ratios=[0.5, 1.0, 2.0],
-            strides=[4, 8, 16, 32, 64]),
+            strides=[16]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
@@ -96,16 +106,25 @@ model = dict(
         loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
     roi_head=dict(
         type='StandardRoIHead',
+        shared_head=dict(
+            type='ResLayer',
+            depth=50,
+            stage=3,
+            stride=2,
+            dilation=1,
+            style='caffe',
+            norm_cfg=norm_cfg,
+            norm_eval=True),
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', out_size=7, sample_num=0),
-            out_channels=256,
-            featmap_strides=[4, 8, 16, 32]),
+            roi_layer=dict(type='RoIPool', out_size=14, use_torchvision=True),
+            out_channels=1024,
+            featmap_strides=[16]),
         bbox_head=dict(
-            type='Shared2FCBBoxHead',
-            in_channels=256,
-            fc_out_channels=1024,
+            type='BBoxHead',
+            with_avg_pool=True,
             roi_feat_size=7,
+            in_channels=2048,
             num_classes=6,
             bbox_coder=dict(
                 type='DeltaXYWHBBoxCoder',
@@ -113,7 +132,7 @@ model = dict(
                 target_stds=[0.1, 0.1, 0.2, 0.2]),
             reg_class_agnostic=False,
             loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.1),
             loss_bbox=dict(type='L1Loss', loss_weight=1.0))))
 # model training and testing settings
 train_cfg = dict(
