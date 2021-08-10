540317c7551f21cafae8166ff9676125f5957c2e
Qianna <892878159@qq.com>
2021-08-10 19:42:07 +0800

    none

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -8,4 +8,4 @@ PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2044 \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/smd_det/normal/faster_rcnn_fpn_5c
-    --resume-from /root/vsislab-2/zq/smd_det/normal/faster_rcnn_fpn_5c/latest ${@:3}
+    --resume-from /root/vsislab-2/zq/smd_det/normal/faster_rcnn_fpn_5c/latest.pth ${@:3}
