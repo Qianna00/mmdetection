bd5e6ffa9fc8d7d031eba458e72149e97f10e174
Qianna <892878159@qq.com>
2021-08-24 14:47:08 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_1x_smd.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_1x_smd.py
@@ -63,7 +63,7 @@ total_epochs = 12
 # model settings
 model = dict(
     type='FasterRCNN',
-    pretrained='/root/data/zq/pretrained_models/moco400k_epoch_50_modified.pth',
+    pretrained='/root/data/zq/pretrained_models/resnet50_fpn.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,5 +6,5 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2040 \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/smd_det/normal/faster_rcnn_fpn_moco400k_5c ${@:3}
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2050 \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/smd_det/normal/faster_rcnn_fpn_5c ${@:3}
