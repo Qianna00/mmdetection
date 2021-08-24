8a49c6fe56d7560919b6f6a5e0c569663f905930
Qianna <892878159@qq.com>
2021-08-24 14:38:03 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_1x_smd.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_1x_smd.py
@@ -57,8 +57,8 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[8, 13])
-total_epochs = 16
+    step=[8, 11])
+total_epochs = 12
 
 # model settings
 model = dict(
