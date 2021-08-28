aed051cb0ca0f6377db2a0f489f3930a68cde69c
Qianna <892878159@qq.com>
2021-08-28 17:02:10 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
@@ -64,7 +64,7 @@ total_epochs = 12
 norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNN',
-    pretrained='/root/data/zq/pretrained_models/resnet50_marvel5_50.pth',
+    pretrained='/root/data/zq/pretrained_models/resnet50_marvel5_25.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,5 +6,5 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2050 \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/smd_det/normal/faster_rcnn_marvel5_sup/epoch50 ${@:3}
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2048 \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/smd_det/normal/faster_rcnn_marvel5_sup/epoch25 ${@:3}
