1fee9fe27a5cfa53ec9afbbfebbc62db247bb8dd
Qianna <892878159@qq.com>
2021-09-01 09:38:49 +0800

    none

--- configs/faster_rcnn_unsup_embedding/faster_rcnn_r50_caffe_fpn_sup_backbone_concat_smd.py
+++ configs/faster_rcnn_unsup_embedding/faster_rcnn_r50_caffe_fpn_sup_backbone_concat_smd.py
@@ -63,8 +63,8 @@ total_epochs = 12
 # model settings
 model = dict(
     type='FasterRCNNWithExtraBackbone',
-    pretrained='/root/data/zq/pretrained_models/moco400k_epoch_50_modified.pth',
-    pretrained_extra='/root/data/zq/pretrained_models/resnet50_marvel_35.pth',
+    pretrained='/root/data/zq/pretrained_models/resnet50_msra.pth',
+    pretrained_extra='/root/data/zq/pretrained_models/resnet50_marvel5_50_new1.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
@@ -79,7 +79,7 @@ model = dict(
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=3,
+        frozen_stages=4,
         norm_cfg=dict(type='BN', requires_grad=False),
         norm_eval=True,
         style='caffe'),
