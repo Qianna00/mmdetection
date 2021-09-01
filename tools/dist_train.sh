ce27a0df95241414d4908e4c5f2a53c56016817d
Qianna <892878159@qq.com>
2021-09-01 14:55:20 +0800

    none

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -7,4 +7,4 @@ PORT=${PORT:-29500}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2048 \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/smd_det/two_backbone/marvel5_imagenet_and_imagenet ${@:3}
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir /root/vsislab-2/zq/smd_det/two_backbone/marvel5_imagenet_and_marvel5_imagenet ${@:3}
