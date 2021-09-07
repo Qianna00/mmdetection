32f8a2b97e99e6b712d0001139c4f7c42823ef43
Qianna <892878159@qq.com>
2021-09-07 15:22:46 +0800

    none

--- configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_smd.py
@@ -64,7 +64,7 @@ total_epochs = 12
 norm_cfg = dict(type='BN', requires_grad=False)
 model = dict(
     type='FasterRCNN',
-    pretrained='/root/data/zq/pretrained_models/resnet50_msra.pth',
+    pretrained='/root/data/zq/pretrained_models/resnet50_marvel5_imagenet_scratch100_modified.pth',
     backbone=dict(
         type='ResNet',
         depth=50,
