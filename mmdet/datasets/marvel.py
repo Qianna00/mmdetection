e7393a36fcd1a4a3846074bdc63a2376d6cabec6
Qianna <892878159@qq.com>
2021-09-27 20:08:12 +0800

    none

--- mmdet/datasets/marvel.py
+++ mmdet/datasets/marvel.py
@@ -11,10 +11,14 @@ from .pipelines import Compose
 
 @DATASETS.register_module()
 class MarvelDataset(Dataset):
-    def __init__(self, ann_file, pipeline, test_mode=False):
+    def __init__(self, ann_file, pipeline, test_mode=False, img_prefix=None, seg_prefix=None, proposal_file=None):
         self.images = []
         self.load_images(ann_file)
         self.pipeline = Compose(pipeline)
+        self.test_mode = test_mode
+        self.img_prefix = img_prefix
+        self.seg_prefix = seg_prefix
+        self.proposal_file = proposal_file
 
     def load_images(self, ann_file):
         fh = open(ann_file, 'r')
@@ -25,7 +29,19 @@ class MarvelDataset(Dataset):
             self.images.append(file_name)
 
     def __getitem__(self, idx):
-        return self.pipeline(self.images[idx])
+        img = self.images[idx]
+        results = dict(img_info=img)
+        self.pre_pipeline(results)
+        return self.pipeline(results)
 
     def __len__(self):
         return len(self.images)
+
+    def pre_pipeline(self, results):
+        results['img_prefix'] = self.img_prefix
+        results['seg_prefix'] = self.seg_prefix
+        results['proposal_file'] = self.proposal_file
+        results['bbox_fields'] = []
+        results['mask_fields'] = []
+        results['seg_fields'] = []
+
