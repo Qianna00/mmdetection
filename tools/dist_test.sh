c79c4d2aa1f4b450e78b3516385f70d0f14aacbd
Qianna <892878159@qq.com>
2021-09-27 19:37:00 +0800

    none

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -7,4 +7,4 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 2201 \
-    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch --format-only ${@:4}
+    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch --out --format-only ${@:4}
